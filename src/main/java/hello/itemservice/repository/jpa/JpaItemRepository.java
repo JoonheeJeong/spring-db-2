e9546e1a7bb7057e5fac0dea4c1d121ac9c4ec85
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-23 14:47:33 +0900

    feat: JpaItemRepository#findAll 구현 (#3)

--- src/main/java/hello/itemservice/repository/jpa/JpaItemRepository.java
+++ src/main/java/hello/itemservice/repository/jpa/JpaItemRepository.java
@@ -5,12 +5,16 @@ import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
+import org.thymeleaf.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Repository
 public class JpaItemRepository implements ItemRepository {
@@ -39,6 +43,39 @@ public class JpaItemRepository implements ItemRepository {
 
     @Override
     public List<Item> findAll(ItemSearchCond cond) {
-        return null;
+        boolean isEmptyItemName = StringUtils.isEmptyOrWhitespace(cond.getItemName());
+        boolean isNullMaxPrice = cond.getMaxPrice() == null;
+        final String jpql = getDynamicQueryForFindAll(isEmptyItemName, isNullMaxPrice);
+        log.info("jpql: {}", jpql);
+
+        TypedQuery<Item> query = em.createQuery(jpql, Item.class);
+        if (!isEmptyItemName) {
+            query.setParameter("itemName", cond.getItemName());
+        }
+        if (!isNullMaxPrice) {
+            query.setParameter("maxPrice", cond.getMaxPrice());
+        }
+        return query.getResultList();
+    }
+
+    private String getDynamicQueryForFindAll(boolean isEmptyItemName, boolean isNullMaxPrice) {
+        StringBuilder sqlBuilder = new StringBuilder("select i from Item i");
+        if (isEmptyItemName && isNullMaxPrice) {
+            return sqlBuilder.toString();
+        }
+
+        StringBuilder whereBuilder = new StringBuilder(" where ");
+        boolean needAnd = false;
+        if (!isEmptyItemName) {
+            whereBuilder.append("item_name like concat('%', :itemName, '%')");
+            needAnd = true;
+        }
+        if (!isNullMaxPrice) {
+            if (needAnd) {
+                whereBuilder.append(" and ");
+            }
+            whereBuilder.append("price <= :maxPrice");
+        }
+        return sqlBuilder.append(whereBuilder).toString();
     }
 }
