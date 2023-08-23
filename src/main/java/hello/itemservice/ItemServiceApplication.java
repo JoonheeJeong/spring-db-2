0440a6bb80219c71c934caa4f0111dbdf883718e
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-23 16:33:50 +0900

    feat: SpringDataJpaConfig 정의 및 등록 (#4)

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,21 +1,18 @@
 package hello.itemservice;
 
-import hello.itemservice.config.*;
-import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.config.SpringDataJpaConfig;
 import hello.itemservice.service.ItemService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
 
 
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateConfig.class)
-@Import(JpaConfig.class)
+//@Import(JpaConfig.class)
+@Import(SpringDataJpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
