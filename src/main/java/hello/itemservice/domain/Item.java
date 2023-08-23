4ea470018352719f9ec13530179dcf72e1640050
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-23 13:58:20 +0900

    feat: Item 도메인 엔티티 설정 (#3)

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -2,13 +2,20 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(length = 10, nullable = false)
     private String itemName;
+    @Column(nullable = false)
     private Integer price;
+    @Column(nullable = false)
     private Integer quantity;
 
     public Item() {
