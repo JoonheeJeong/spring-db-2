b2cc571e3983a01400aad958c7c8869089f17be4
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-23 13:57:32 +0900

    feat: JpaItemRepository 일부 구현 (#3)
    - save
    - update
    - findById

---
+++ src/main/java/hello/itemservice/repository/jpa/JpaItemRepository.java
@@ -0,0 +1,44 @@
+package hello.itemservice.repository.jpa;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Repository
+public class JpaItemRepository implements ItemRepository {
+
+    private final EntityManager em;
+
+    @Override
+    public Item save(Item item) {
+        em.persist(item);
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        Item item = em.find(Item.class, itemId);
+        item.setItemName(updateParam.getItemName());
+        item.setPrice(updateParam.getPrice());
+        item.setQuantity(updateParam.getQuantity());
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        Item item = em.find(Item.class, id);
+        return Optional.ofNullable(item);
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        return null;
+    }
+}
