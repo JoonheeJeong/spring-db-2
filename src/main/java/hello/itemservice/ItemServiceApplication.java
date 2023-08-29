73b20f22f07c4e8cfe2389d3d6adc73bed508951
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-29 11:31:52 +0900

    feat: 설정 파일 작성 및 등록 (#5)

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,6 +1,7 @@
 package hello.itemservice;
 
 import hello.itemservice.config.QueryDslConfig;
+import hello.itemservice.config.V2Config;
 import hello.itemservice.service.ItemService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,7 +14,8 @@ import org.springframework.context.annotation.Profile;
 //@Import(JdbcTemplateConfig.class)
 //@Import(JpaConfig.class)
 //@Import(SpringDataJpaConfig.class)
-@Import(QueryDslConfig.class)
+//@Import(QueryDslConfig.class)
+@Import(V2Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
