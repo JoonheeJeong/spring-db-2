24d6c1797944c9d4303785dfebfea79cf1c09c15
Joonhee Jeong <jeonggoo75@gmail.com>
2023-09-04 12:29:01 +0900

    test(chore): 불필요 import 제거

--- springtx/src/test/java/hello/springtx/apply/InternalCallV1Test.java
+++ springtx/src/test/java/hello/springtx/apply/InternalCallV1Test.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 @SpringBootTest
 public class InternalCallV1Test {
--- springtx/src/test/java/hello/springtx/apply/TxBasicTest.java
+++ springtx/src/test/java/hello/springtx/apply/TxBasicTest.java
@@ -1,8 +1,6 @@
 package hello.springtx.apply;
 
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
