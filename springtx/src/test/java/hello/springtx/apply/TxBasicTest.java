47d81b14a397703fa7a951c88e7a05915e142a03
Joonhee Jeong <jeonggoo75@gmail.com>
2023-09-04 11:43:18 +0900

    test: 트랜잭션 aop 활성 상태 확인 (#6)

--- springtx/src/test/java/hello/springtx/apply/TxBasicTest.java
+++ springtx/src/test/java/hello/springtx/apply/TxBasicTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,6 +25,12 @@ public class TxBasicTest {
     void proxyCheck() {
         log.info("aop class={}", basicService.getClass());
         assertThat(AopUtils.isAopProxy(basicService)).isTrue();
+    }
+
+    @Test
+    void testTx() {
+        basicService.tx();
+        basicService.nonTx();
     }
 
     @TestConfiguration
@@ -41,10 +48,14 @@ public class TxBasicTest {
         @Transactional
         public void tx() {
             log.info("call tx");
+            boolean isActualTransactionActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active: {}", isActualTransactionActive);
         }
 
         public void nonTx() {
             log.info("call nonTx");
+            boolean isActualTransactionActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active: {}", isActualTransactionActive);
         }
     }
 
